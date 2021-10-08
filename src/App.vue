8ca883a0dbd7a886ceabb0fa51057f211bfec28a
Somya Agarwal <somya.agarwal@joshtechnologygroup.com>
2021-10-08 12:38:34 +0530

    [RU-0] completed base setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="container">Hello World</h1>
+</template>
+
+<style lang="scss">
+@import './sass/abstracts/variables';
+@import './sass/abstracts/mixin';
+@import './sass/base/common';
+@import './sass/base/reset';
+@import './sass/base/typography';
+@import './sass/vendors/fonts';
+@import './sass/vendors/container';
+</style>
